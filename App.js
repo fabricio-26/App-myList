b31d158efb37c83cef767b59041121c6ff4c0b28
fabricio henrique alves <fabricioohh@gmail.com>
2022-11-10 06:42:20 -0300

    style tela de tarefas

--- App.js
+++ App.js
@@ -1,34 +1,92 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  SafeAreaView, 
+  TextInput, 
+  TouchableOpacity, 
+  FlatList
+} from 'react-native';
 
-import Login from './src/components/Login/index'
+import Login from './src/components/Login';
+import TaskList from './src/components/TaskList';
+console.disableYellowBox = true;
 
-console.disableYellowBox=true;
+let tasks = [
+  {key: '1', nome: "Comprar Coca Cola"},
+  {key: '2', nome: "Estudar Js"}
+]
 
 export default function App() {
   const [user, setUser] = useState(null)
-  
- 
+  const [newTask, setNewTask] = useState('')
 
 
 
-  if(!user){
-    return <Login thisUserStatus={ (user) => setUser(user) }/>
+
+
+  if (!user) {
+    return <Login thisUserStatus={(user) => setUser(user)} />
   }
 
- return (
-   <SafeAreaView style={styles.container}>
-    <Text>DENTRO DA TELA DE TAREFAS</Text>
-   </SafeAreaView>
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerTask}>
+        <TextInput
+          placeholder='O que va fazer hoje?'
+          value={newTask}
+          onChangeText={ (text) => setNewTask(text)}
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.buttonAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+      data={tasks}
+      keyExtractor={item => item.key}
+      renderItem={({item}) => <TaskList data={item}/>}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    paddingTop:25,
+    paddingTop: 25,
     paddingHorizontal: 10,
-    backgroundColor:'#f2f6fc'
+    backgroundColor: '#f2f6fc'
   },
-  
+  containerTask:{
+    flexDirection:'row',
+  },
+  input:{
+    flex:1,
+    marginBottom:10,
+    padding:10,
+    backgroundColor:"#fff",
+    borderRadius:4,
+    borderWidth:1,
+    borderColor:'#141414',
+    height:45
+  },
+  buttonAdd:{
+    backgroundColor:'#141414',
+    color:'#fff',
+    paddingHorizontal:15,
+    height: 45,
+    marginStart: 5,
+    alignItems: 'center',
+    justifyContent:'center',
+    borderRadius: 4
+  },
+  buttonText:{
+    color:'#fff',
+    fontSize:25
+  }
+
 })
---
+++ src/components/TaskList/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather'
+
+export default function TaskList({data}) {
+ return (
+   <View style={styles.container}>
+    <TouchableOpacity style={{marginRinght: 10}}>
+        <Feather name="trash" color="#fff" size={20}/>
+    </TouchableOpacity>
+
+    <View style={{paddingRight:10}}>
+        <TouchableWithoutFeedback>
+            <Text style={{color:'#fff', paddingRight: 10, marginLeft: 10}}>{data.nome}</Text>
+        </TouchableWithoutFeedback>
+    </View>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:'row',
+        backgroundColor: '#141414',
+        marginBottom: 10,
+        padding:10,
+        borderRadius: 5,
+        alignItems: 'center'
+    }
+})
